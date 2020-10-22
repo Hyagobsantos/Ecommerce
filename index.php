6691da4e8a208109a5f9540d194904d760f644cf
Hyagobsantos <hyagobezerra64@gmail.com>
2020-10-22 10:10:38 -0300

    criando pagaAdmin e tplAdmin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\pageAdmin;
 
 $app = new Slim();
 
@@ -12,6 +13,14 @@ $app->config('debug', true);
 $app->get('/', function() { // o primeiro parametro / indica que nâo foi passado nemhuma rota ele entra e carrega a pagina principal
     
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() { // o primeiro parametro / indica que nâo foi passado nemhuma rota ele entra e carrega a pagina principal
+    
+	$page = new pageAdmin();
 
 	$page->setTpl("index");
 
