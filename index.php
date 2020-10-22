674b9449c0d3b432d746d13cbac3c8afff2c413d
Hyagobsantos <hyagobezerra64@gmail.com>
2020-10-22 11:24:38 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\pageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -20,9 +23,39 @@ $app->get('/', function() { // o primeiro parametro / indica que nâo foi passad
 
 $app->get('/admin', function() { // o primeiro parametro / indica que nâo foi passado nemhuma rota ele entra e carrega a pagina principal
     
+	User::verifyLogin();
+
 	$page = new pageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login' , function() {
+
+	$page = new pageAdmin([
+
+		"header"=>false,
+		"footer"=>false
+
+	]);
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {  //rota a qual faz todas os passos para dps entrar na parte admin 
+
+		User::login($_POST["login"], $_POST["password"]);
+
+		header("Location: /admin");
+		exit;
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
 
 });
 
